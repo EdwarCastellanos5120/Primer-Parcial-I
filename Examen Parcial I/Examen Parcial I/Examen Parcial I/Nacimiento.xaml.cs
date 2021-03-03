1609fad2e9c3505c3a0f269e151da30a16cb9dc8
Edwar Castellanos Portillo <ecastellanosp1@gmail.com>
2021-03-03 15:30:55 -0600

    Primer Parcial I
    Proyecto Primer Parcial

---
+++ Examen Parcial I/Examen Parcial I/Examen Parcial I/Nacimiento.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_Parcial_I
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Nacimiento : ContentPage
+    {
+        public Nacimiento()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Clicked(object sender, EventArgs e)
+        {
+           int  resta, dia = Convert.ToInt32(n01.Text);
+            string resul;
+            resta = dia - 365;
+            if (resta <= 1000)
+            {
+                string nuevo = Convert.ToString(resta);
+                string n2 = nuevo.Substring(1, 2);
+                resul=("Su dia es: " + nuevo[0]+ " Su numero de mes : " + n2);
+            }
+            else
+            {
+                string nuevo = Convert.ToString(resta);
+                string n1 = nuevo.Substring(0, 2);
+                string n2 = nuevo.Substring(2, 2);
+                resul=("Su dia es: " + n1+ " Su numero de mes : " + n2);
+            }
+            DisplayAlert("Su Fecha de N", resul, "Ok");
+        }
+    }
+}
---
+++ Examen Parcial I/Examen Parcial I/Examen Parcial I/Numero.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Examen_Parcial_I
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Numero : ContentPage
+    {
+        public Numero()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Clicked(object sender, EventArgs e)
+        {
+             string numero = Convert.ToString(numero01.Text);
+            string numero2 = numero.Substring(0, numero.Length - 1);
+            int resul, resultado = Convert.ToInt32(numero2);
+            resul = resultado - 4;
+            string re = Convert.ToString(resul);
+            DisplayAlert("Su Numero es:", re, "Ok");
+        }
+    }
+}
